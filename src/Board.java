3cd1604a0a1b5ab8320419fcefdaf30dba8a7f34
Aaron Tong <ytong3@utk.edu>
2014-04-07 01:07:58 -0400

    adding configure and map class of Nqueens

--- src/Board.java
+++ src/Board.java
@@ -9,20 +9,31 @@ public class Board {
   private int dimension; //the dimension of the board
   private List<String> rows;
 
+/*
   public Board(int dim){
     dimension = dim;
     rows = new ArrayList<String>();
   }
+*/
   public Board(String boardLayout ) {
     String[] tempRows = boardLayout.split("|");
     rows.addAll(tempRows);
+    dimension = tempRows.length;
   }
 
-  public toString(){
+  public String toString(){
     String str="";
     for (String row:rows){
       str=str+row+"|";
     }
     return str;
+  }
+
+  private Board[] generateNewBoard(){
+    for (int col = 0;col<dimension;col++){
+      //check if there is conflict in the same column
+
+      //check if there there are conflicts in the diagonal direction
+    }
   }
 }
