56b0612e149145e00dcbb0483587516fe5c11192
Yue Tong <ytong3@utk.edu>
2014-04-09 16:02:28 -0400

    debugging partial result

--- src/Board.java
+++ src/Board.java
@@ -1,6 +1,5 @@
+package nqueens.util;
 import java.util.*;
-
-import org.apache.hadoop.io.Text;
 
 // Board classes are to represent the square chessboards.
 // A Board object is not not complete unless it has the same width and depth
@@ -8,7 +7,7 @@ import org.apache.hadoop.io.Text;
 
 public class Board {
   private int dimension; //the dimension of the board
-  private List<Integer> queenPos;
+  private List<Integer> queensPos;
 
 /*
   public Board(int dim){
@@ -24,24 +23,27 @@ public class Board {
   }
 
   public String toString(){
-    String str=StringUtils.join(queenPos,",");
+    String str=StringUtils.join(queensPos,",");
     return str;
   }
 
   public List<String> generateNewBoards(){
-    //if queenPos is not initialized, throw an exception
-    //if queenPos already has enough number of elements, halt.
+    //if queensPos is not initialized, throw an exception
+    //if queensPos already has enough number of elements, halt.
 
-    boolean[] cells = new boolean[dimension]{true};
-    for (int row = 0;row<queenPos.size();row++){
+    boolean[] cells = new boolean[dimension];
+	for(int i=0;i<cells.length;i++)
+		cells[i]=true;
+	
+    for (int row = 0;row<queensPos.size();row++){
         //eliminate all existing columns
-        cells[queenPos.get(row)]=false;
+        cells[queensPos.get(row)]=false;
 
         //eliminate cells that in the diagnal
-        int leftDiagonal = queenPos.get(row) - (queensPos.size()-row);
-        int rightDiagonal = queenPos.get(row) + (queensPos.size()-row);
+        int leftDiagonal = queensPos.get(row) - (queensPos.size()-row);
+        int rightDiagonal = queensPos.get(row) + (queensPos.size()-row);
 
-        if (leftDiagonal>=0) cells[leftDiagnonal]=false;
+        if (leftDiagonal>=0) cells[leftDiagonal]=false;
         if (rightDiagonal<=dimension) cells[rightDiagonal]=false;
     }
 
@@ -50,7 +52,7 @@ public class Board {
 
     for (int col =0;col<dimension;col++){
         if (cells[col]==true){
-            res.add(oldBoard.append(","+Integer.toString(col)));
+            res.add(oldBoard.concat(","+Integer.toString(col)));
         }
     }
     return res;
